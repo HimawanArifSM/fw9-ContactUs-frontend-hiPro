875e9732d97b69123f31c2c155c7eb5151cea431
HimawanArifSM <himawanarif7@gmail.com>
2022-09-27 16:51:38 +0700

    slicing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import{Routes, Route, BrowserRouter} from 'react-router-dom'
+
+import Contactus from "./pages/ContactUs"
+import GetAllContactus from "./pages/GetAllContactus"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Contactus />} />
+        <Route path="/get-all-contactus" element={<GetAllContactus />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {FiMapPin,FiMail,FiPhone} from "react-icons/fi"
+import { Button, Form } from 'react-bootstrap';
+import * as Yup from 'yup';
+import { Formik } from "formik";
+import { useDispatch } from "react-redux";
+
+const contactUsSchema = Yup.object().shape({
+    fullname: Yup.string().length({min: 4}).required('Required'),
+    email: Yup.string().email("Invalid email format").required('Required'),
+    messages: Yup.string().length({max: 255}).required('Required')
+})
+
+function ContactUsForm(props){
+    return(
+        <Form className="flex flex-col gap-10">
+            <Form.Group className="border-b-[1px] border-black pb-2 outline-hidden">
+                <Form.Control isInvalid={!!props.errors.email} onChange={props.handleChange} name="fullname" className="outline-none w-full" placeholder="Name" />
+                <Form.Control.Feedback type="invalid">{props.errors.fullname}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="border-b-[1px] border-black pb-2 outline-hidden">
+                <Form.Control isInvalid={!!props.errors.email} onChange={props.handleChange} name="email" className="outline-none w-full"placeholder="E-mail" />
+                <Form.Control.Feedback type="invalid">{props.errors.email}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="border-b-[1px] border-black pb-2 outline-hidden">
+            <textarea isInvalid={!!props.errors.email} onChange={props.handleChange} name="messages" className="w-full h-full outline-none" form="messages" placeholder="Messages" ></textarea>
+                <Form.Control.Feedback type="invalid">{props.errors.messages}</Form.Control.Feedback>
+            </Form.Group>
+            <div className="flex justify-center "><Button type='submit' className="bg-green-600 pl-20 pr-20 pt-3 pb-3">SEND</Button></div>
+        </Form>
+    )
+}
+
+function ContactUs() {
+    const dispatch = useDispatch()
+    const onSubmit = (value) => {
+        const data = { fullname: value.fullname, email: value.email, messages: value.messages };
+        // dispatch(contactUs(data));
+        console.log(data);
+      };
+
+    return(
+        <div className="lp-background min-h-screen ">
+            <div className="flex justify-evenly bg-black/75 min-h-screen p-5 items-center">
+                <div className="flex flex-col gap-10">
+                    <div  className="flex-row flex gap-3">
+                        <FiMapPin size={36} color="white" />
+                        <div>
+                            <h1 className="text-white">ADDRESS</h1>
+                            <p className="text-white">71 Pilgrim Avenue Chevy Chase, MD 20815</p>
+                        </div>
+                    </div>
+                    <div className="flex-row flex gap-3">
+                        <FiPhone size={36} color="white" />
+                        <div>
+                            <h1 className="text-white">PHONE</h1>
+                            <p className="text-white">+713-621-7636</p>
+                        </div>
+                    </div>
+                    <div className="flex-row flex gap-3">
+                        <FiMail size={36} color="white" />
+                        <div>
+                            <h1 className="text-white">EMAIL</h1>
+                            <p className="text-white">thompson@dayrep.com</p>
+                    </div>
+                    </div>
+                </div>
+                <div className="bg-white p-16 flex flex-col gap-10">
+                    <h1 className="text-green-600 font-bold flex justify-center">LEAVE US MESSAGES</h1>
+                    <Formik 
+                    onSubmit={onSubmit}
+                    initialValues={{email: '', password:''}} validationSchema={contactUsSchema}>
+                    {(props)=><ContactUsForm {...props}/>}
+                    </Formik>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ContactUs
